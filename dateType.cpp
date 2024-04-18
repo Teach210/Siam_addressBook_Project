bc01df0a2f1311f513b66a57ff05d6d12c397488
Teach210 <sss20802@email.vccs.edu>
2024-04-18 12:39:36 -0400

    Before default constructor

--- dateType.cpp
+++ dateType.cpp
@@ -1,1 +1,57 @@
 #include "dateType.h"
+#include <iostream>
+
+dateType::dateType(int d, int m, int y)
+{
+	setDate(d, m, y); // Valadates and sets the date.
+}
+
+void dateType::setDate(int d, int m, int y)
+{
+	// Checks Year
+	if (y < 1900)
+		dYear = 1900;
+	else
+		dYear = y;
+
+	// Checks month
+	if (m < 1 || m > 12)
+		dMonth = 1;
+	else
+		dMonth = m;
+
+
+	// Checks day
+	int lastDayOfMonth = daysInMonth(dMonth, dYear);
+	if (d < 1 || d > lastDayOfMonth)
+		dDay = 1;
+	else
+		dDay = d;
+}
+
+int dateType::getDay() const { return dDay; }
+int dateType::getMonth() const { return dMonth; }
+int dateType::getYear() const { return dYear; }
+void dateType::print() const { std:: cout << dMonth << "/" << dDay << "/" << dYear << std::endl; }
+bool dateType::isLeapYear(int year) const { return (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0); }
+int dateType:: daysInMonth(int month, int year) const 
+{
+	switch (month)
+	{
+		case 2:
+			if (isLeapYear(year))
+				return 29;
+			else
+				return 28;
+		case 4:
+		case 6:
+		case 9:
+		case 11:
+			return 30;
+		default:
+			return 31;
+
+
+	}
+}
+
--- dateType.h
+++ dateType.h
@@ -1,5 +1,31 @@
 #pragma once
+#include <iostream>
+
 class dateType
 {
+private:
+	int dDay = 1;
+	int dMonth = 1;
+	int dYear = 1900;
+
+public:
+	// Constructor//
+	dateType(int d, int m, int y);
+
+	// Setter
+	void setDate(int d, int m, int y);
+
+	// Getter
+	int getDay() const;
+	int getMonth() const;
+	int getYear() const;
+
+	// Other
+
+	void print() const;
+	bool isLeapYear(int Year) const;
+	int daysInMonth(int month, int year) const;
+
+
 };
 
