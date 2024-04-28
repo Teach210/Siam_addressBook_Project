0beea5d173c3279b6ddb3535d397ed8c706d2e50
Teach210 <sss20802@email.vccs.edu>
2024-04-28 09:43:15 -0400

    Started extPersonType Class

--- Siam_addressBook_Project.cpp
+++ Siam_addressBook_Project.cpp
@@ -1,18 +1,23 @@
 #include <iostream>
-#include "personType.h"
-
-int main() {
-    // Creating an instance of personType
-    personType person("John", "Doe");
-
-    // Printing person's information
-    person.print();
-
-    // Updating person's first name
-    person.setFirstName("Jane");
-
-    // Printing updated information
-    person.print();
-
-    return 0;
+#include "extPersonType.h"
+using namespace std;
+int main()
+{
+	cout << "Testing default constructor ... " << endl;
+	extPersonType defPerson;
+	defPerson.print();
+	cout << endl;
+	cout << "Testing constructor with parameters ... " << endl;
+	extPersonType person("George", "Smith", 4, 30, 1994, "145 South Street, Apt.
+		5", "Hampton", "VA", 23554, "757 - 444 - 5555", "Friend");
+		person.print();
+	cout << endl;
+	cout << "Testing invalid relationship (Spouse) ... " << endl;
+	person.setRelationship("Spouse");
+	person.print();
+	cout << endl;
+	cout << "Testing valid relationship (Business) ... " << endl;
+	person.setRelationship("Business");
+	person.print();
+	cout << endl;
 }
